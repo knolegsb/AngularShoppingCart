15e71d3603536a133261dd33f006e517f7204203
Sean <seon2you@hotmail.com>
2016-08-09 00:20:39 -0700

    adding shopping cart

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,1 +1,60 @@
-﻿
+﻿function shoppingCart(cartName) {
+    this.cartName = cartName;
+    this.clearCart = false;
+    this.checkoutParameters = {};
+    this.items = [];
+
+    this.loadItems();
+
+    var self = this;
+    $(window).unload(function () {
+        if (self.clearCart) {
+            self.clearItems();
+        }
+        self.saveItmes();
+        self.clearCart = false;
+    });
+}
+
+shoppingCart.prototype.loadItems = function () {
+    var items = localStorage != null ? localStorage[this.cartName + "_items"] : null;
+    if (items != null && JSON != null) {
+        try {
+            var items = JSON.parse(items);
+            for (var i = 0; i < items.length; i++){
+                var item = items[i];
+                if (item.sku != null && item.name != null && item.price != null && item.quantity != null){
+                    item = new cartItem(item.sku, item.name, item.price, item.quantity);
+                    this.items.push(item);
+                }
+            }
+        }
+        catch (err) {
+
+        }
+    }
+}
+
+shoppingCart.prototype.addItem = function (sku, name, price, quantity) {
+    quantity = this.toNumber(quantity);
+    if (quantity != 0) {
+        var found = false;
+        for (var i = 0; i < this.items.length && !found; i++) {
+            var item = this.items[i];
+            if (item.sku == sku) {
+                found = true;
+                item.quantity = this.toNumber(item.quantity + quantity);
+                if (item.quantity <= 0) {
+                    this.items.splice(i, 1);
+                }
+            }
+        }
+
+        if (!found) {
+            var item = new cartItem(sku, name, price, quantity);
+            this.items.push(item);
+        }
+
+        this.saveItems();
+    }
+}
